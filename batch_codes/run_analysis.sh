2ba58da98d93ef14e379a03d3f16d7eb0b5fac71
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-08-27 01:32:26 -0400

    add run code for batched all analysis in analysis folder

---
+++ batch_codes/run_analysis.sh
@@ -0,0 +1,23 @@
+python spont_activity.py
+python spont_plt_spk.py
+
+python Rmax.py
+
+python oscillations_run.py
+
+python movies_plt_spk.py
+
+python flashes_frt.py
+python flashes_plt_spk.py
+
+python gratings_plt_spk.py
+
+python f_rate_hist.py
+
+python Ori_compute.py
+python Ori_plot.py
+python Ori_correlations_input.py
+python Ori_tuning_curves_Gaussian_fit.py
+python Ori_tuning_curves.py
+
+python calc_sparseness.py
