485ca7be284f33721d64babc7297c713962775de
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-08-27 01:13:41 -0400

    remove script to generate local ll2 system files

--- ll2_system_files_and_scripts/simulate_input.sh
+++
@@ -1,8 +0,0 @@
-# cd LGN/
-python generate-LGN-rates.py
-python generate-LGN-spikes.py
-
-cd ../tw_data/ll2_tw_build/
-python run_traveling_waves.py
-# This seems duplicated
-# python run_traveling_waves.py
