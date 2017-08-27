a14910fee766bed22edff1855e0dab0f4eb5b7c6
Ziqiang Wei <weiz@janelia.hhmi.org>
2017-08-27 14:04:51 -0400

    sync bio2lif_model_intFire4_ll with server

---
+++ training_codes/bio2lif_model_intFire4_ll/auto_run_qsub_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python qsub_combined_flash_001.py
+python qsub_combined_flash_002.py
+python qsub_combined_grating_001.py
+python qsub_combined_grating_002.py
+python qsub_combined_grating_003.py
+python qsub_combined_spont_001.py
+python qsub_combined_stim_001.py
+python qsub_combined_stim_002.py
